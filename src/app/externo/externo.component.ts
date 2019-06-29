573a34c41924463b2e1f27993c7e61b66392153b
elkingutierrex <elkin.gutierrex@gmail.com>
2019-06-29 12:00:10 -0500

    195 - Efecto de carga

--- src/app/externo/externo.component.ts
+++ src/app/externo/externo.component.ts
@@ -23,6 +23,7 @@ export class ExternoComponent implements OnInit {
   }
 
   cargaUsuario(){
+    this.user = false;
     this._peticionesService.getUser(this.userId).subscribe(
       result => {
         this.user = result.data;
